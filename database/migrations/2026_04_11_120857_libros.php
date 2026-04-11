5018bcec446ea2cb4b8153c7ddf7663d6e093c34
unknown <almanzaisrael75@gmail.com>
2026-04-11 08:32:08 -0400

    instalacion de ui y boostrap

--- database/migrations/2026_04_11_120857_libros.php
+++ database/migrations/2026_04_11_120857_libros.php
@@ -12,6 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('precio');
+            $table->timestamps();
+        });
     }
 
     /**
