f596de490e5b0e18f16061e2a5df51644e012487
unknown <almanzaisrael75@gmail.com>
2026-04-11 09:42:49 -0400

    base 2

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
+Route::resource('libros', App\Http\Controllers\LibroController::class)->middleware('auth');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
