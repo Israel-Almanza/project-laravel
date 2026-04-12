745bd0a056113afc77bea7a1361355bc168f936e
unknown <almanzaisrael75@gmail.com>
2026-04-12 14:02:15 -0400

    cofig cors v2

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array<int, string>|string|null
+      /**
+     * 🔥 Confiar en TODOS los proxies (clave en Render)
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
